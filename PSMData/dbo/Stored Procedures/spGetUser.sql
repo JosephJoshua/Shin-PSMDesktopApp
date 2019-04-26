bf8ea8eadd963f3ea7615b4f042a0149b8ed19d5
JosephJoshua Anggita <jj.anggita@gmail.com>
2019-04-26 17:02:13 +0900

    Create class library for API and add a new endpoint
    Creates the class library for the API and add a new endpoint to get the User by id.

---
+++ PSMData/dbo/Stored Procedures/spGetUser.sql
@@ -0,0 +1,10 @@
+﻿create procedure [dbo].[spGetUser]
+	@Id nvarchar(128)
+as
+begin
+	set nocount on;
+
+	select [Id], [Username], [EmailAddress], [Role]
+	from [dbo].[User]
+	where Id = @Id;
+end
---
+++ PSMDataManager.Library/Models/UserModel.cs
@@ -0,0 +1,13 @@
+﻿namespace PSMDataManager.Library.Models
+{
+    public class UserModel
+    {
+        public string Id { get; set; }
+
+        public string Username { get; set; }
+
+        public string EmailAddress { get; set; }
+
+        public string Role { get; set; }
+    }
+}
