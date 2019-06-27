0b2faf9665d7406f6006c167dc5fca19cc8c340c
JosephJoshua Anggita <jj.anggita@gmail.com>
2019-06-28 02:39:35 +0900

    Add stored procedures for Service table
    Adds stored procedures for the Service table to do CRUD operations.

---
+++ PSMData/dbo/Stored Procedures/spDeleteService.sql
@@ -0,0 +1,9 @@
+﻿create procedure [dbo].[spDeleteService]
+	@NomorNota int
+as
+begin
+	set nocount on;
+
+	delete from [dbo].[Service]
+	where [NomorNota] = @NomorNota;
+end
---
+++ PSMData/dbo/Stored Procedures/spInsertService.sql
@@ -0,0 +1,36 @@
+﻿create procedure [dbo].[spInsertService]
+(
+	@NamaPelanggan nvarchar(50),
+	@NoHp varchar(12),
+	@TipeHp nvarchar(50),
+	@Imei varchar(4),
+	@DamageId int,
+	@YangBelumDicek nvarchar(50),
+	@Kelengkapan varchar(50),
+	@Warna varchar(50),
+	@KataSandiPola varchar(50),
+	@TechnicianId int,
+	@StatusServisan varchar(50),
+	@TanggalKonfirmasi datetime2,
+	@IsiKonfirmasi nvarchar(256),
+	@Biaya decimal(19, 4),
+	@Discount int,
+	@Dp decimal(19, 4),
+	@TambahanBiaya decimal(19, 4),
+	@HargaSparepart decimal(19, 4),
+	@LabaRugi decimal(19, 4),
+	@TanggalPengambilan datetime2
+)
+as
+begin
+	set nocount on;
+
+	insert into [dbo].[Service]
+	(NomorNota, NamaPelanggan, NoHp, TipeHp, Imei, DamageId, YangBelumDicek, Kelengkapan, Warna, KataSandiPola, TechnicianId, StatusServisan, TanggalKonfirmasi,
+	 IsiKonfirmasi, Biaya, Discount, Dp, TambahanBiaya, TotalBiaya, HargaSparepart, Sisa, LabaRugi, TanggalPengambilan)
+	values
+	(next value for [dbo].[serviceIdSequence], @NamaPelanggan, @NoHp, @TipeHp, @Imei, @DamageId, @YangBelumDicek, @Kelengkapan, @Warna, @KataSandiPola, @TechnicianId,
+	 @StatusServisan, @TanggalKonfirmasi, @IsiKonfirmasi, @Biaya, @Discount, @Dp, @TambahanBiaya, 
+	 (@Biaya - (@Biaya * (cast(@Discount as decimal(19, 4)) / 100))) + @TambahanBiaya, @HargaSparepart, (@Biaya - (@Biaya * (cast(@Discount as decimal(19, 4)) / 100))) + @TambahanBiaya - @Dp,
+	 @LabaRugi, @TanggalPengambilan);
+end
