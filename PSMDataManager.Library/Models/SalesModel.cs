a275ebf53ff369509e8bd8bfdbf4d1444c32d8e1
JosephJoshua <jj.anggita@gmail.com>
2021-05-16 16:53:19 +0900

    Remove ASP.NET API from the solution

--- PSMDataManager.Library/Models/SalesModel.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace PSMDataManager.Library.Models
-{
-    public class SalesModel
-    {
-        public int Id { get; set; }
-
-        public string Nama { get; set; }
-    }
-}
--- PSMDataManager/Areas/HelpPage/ModelDescriptions/SimpleTypeModelDescription.cs
+++
@@ -1,6 +0,0 @@
-namespace PSMDataManager.Areas.HelpPage.ModelDescriptions
-{
-    public class SimpleTypeModelDescription : ModelDescription
-    {
-    }
-}
