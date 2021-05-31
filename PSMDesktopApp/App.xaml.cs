f7f71bc41666c256f0fea74d5d839dd42f76325e
JosephJoshua <jj.anggita@gmail.com>
2021-05-31 15:10:23 +0900

    Remove handling of unhandled exceptions in App.xaml.cs

--- PSMDesktopApp/App.xaml.cs
+++ PSMDesktopApp/App.xaml.cs
@@ -1,39 +1,8 @@
 using System.Windows;
-using System.Windows.Threading;
 
 namespace PSMDesktopApp
 {
     public partial class App : Application
     {
-        public App()
-        {
-            Dispatcher.UnhandledException += OnDispatcherUnhandledException;
-        }
-
-        private void OnDispatcherUnhandledException(object sender, DispatcherUnhandledExceptionEventArgs e)
-        {
-            #if DEBUG
-
-            e.Handled = false;
-
-            #else
-
-            e.Handled = true;
-
-            string errorMessage = "";
-
-            if (e.Exception.InnerException == null)
-            {
-                errorMessage = string.Format("An application error has occured: {0}", e.Exception.ToString());
-            }
-            else
-            {
-                errorMessage = string.Format("An application error has occured: {0} Inner exception: {1}", e.Exception.ToString(), e.Exception.InnerException.ToString());
-            }
-
-            MessageBox.Show(errorMessage, "Application error", MessageBoxButton.OK, MessageBoxImage.Error);
-
-            #endif
-        }
     }
 }
