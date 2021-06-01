3c6f2b28b8385179a665ea2975da3251e608a501
JosephJoshua <jj.anggita@gmail.com>
2021-06-01 21:24:52 +0900

    Handle all uncaught exceptions and log them

--- PSMDesktopApp/App.xaml.cs
+++ PSMDesktopApp/App.xaml.cs
@@ -1,8 +1,40 @@
+using PSMDesktopApp.Utils;
+using System;
+using System.Threading.Tasks;
 using System.Windows;
 
 namespace PSMDesktopApp
 {
     public partial class App : Application
     {
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            base.OnStartup(e);
+
+            SetupExceptionHandling();
+        }
+
+        private void SetupExceptionHandling()
+        {
+            AppDomain.CurrentDomain.UnhandledException += (s, e) => LogUnhandledException(s, e.ExceptionObject as Exception);
+
+            DispatcherUnhandledException += (s, e) =>
+            {
+                LogUnhandledException(s, e.Exception);
+                e.Handled = true;
+            };
+
+            TaskScheduler.UnobservedTaskException += (s, e) =>
+            {
+                LogUnhandledException(s, e.Exception);
+                e.SetObserved();
+            };
+        }
+
+        private void LogUnhandledException(object sender, Exception exception)
+        {
+            var exceptionLogger = new NLogLogger(sender.GetType());
+            exceptionLogger.Error(exception);
+        }
     }
 }
