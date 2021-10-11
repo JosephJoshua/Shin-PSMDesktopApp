c9eaa4383fb0205ad9fc675e09a468757ddf8d3e
JosephJoshua <jj.anggita@gmail.com>
2021-10-11 21:55:46 +0700

    Checks to see if we can establish a connection to the API or not

---
+++ PSMDesktopApp.Library/Helpers/IConnectionHelper.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace PSMDesktopApp.Library.Helpers
+{
+    public interface IConnectionHelper
+    {
+        Action OnConnectionFailed { set; }
+
+        bool CanConnectToApi();
+    }
+}
